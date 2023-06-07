c8574f57b9382350342903712836cc6fde544f0d
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-06-07 22:22:08 +0300

    fix11

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -59,7 +59,7 @@ const Contact = () => {
           setLoading(false);
           console.error(error);
 
-          alert("Ahh, something went wrong. Please try again.");
+          alert("Ahh, something went wrong. Please try again!");
         }
       );
   };
